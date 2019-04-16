d15b2f19c5a5bbcc56ede7fe164ed0e1ee4e5b5a
Chris Lawrence <lawrencc@debian.org>
2019-04-16 13:46:51 -0400

    Code cleanups.

--- watch-gps.py
+++ watch-gps.py
@@ -2,8 +2,6 @@
 
 # Uses: dot3k, gps
 
-USE_METRIC = True
-#USE_METRIC = False
 EMULATE = False
 
 if EMULATE:
@@ -14,6 +12,20 @@ else:
 import gpsd
 import time
 import math
+
+USE_METRIC = True
+#USE_METRIC = False
+
+if USE_METRIC:
+    distance_unit = "m"
+    distance_factor = 1.0
+    speed_unit = "km/h"
+    speed_factor = 3.6
+else:
+    distance_unit = "'"
+    distance_factor = 3.28084
+    speed_unit = "mph"
+    speed_factor = 2.23694
 
 connected = False
 while not connected:
@@ -34,7 +46,7 @@ while True:
     #lcd.set_cursor_position(11,2)
     #lcd.write('%2d/%02d' % (packet.sats_valid, packet.sats))
 
-    lcd.set_cursor_position(13,2)
+    lcd.set_cursor_position(13, 2)
     lcd.write('%2d@' % (packet.sats_valid))
 
     if packet.mode >= 2:
@@ -49,36 +61,30 @@ while True:
         lcd.set_cursor_position(0,1)
         lcd.write('%9.5f' % (abs(lon)) + ew)
         lcd.set_cursor_position(0,2)
-        if USE_METRIC:
-            speed = packet.speed() * 3.6
-            units = 'km/h'
-        else:
-            speed = packet.speed() * 2.23694
-            units = 'mph'
-        direction = ('NW', 'N', 'NE', 'E', 'SE', 'S', 'SW', 'W')[int((packet.track + 22.5)/45.0) % 8]
+        speed = packet.speed * speed_factor
+        direction = ('N', 'NE', 'E', 'SE', 'S', 'SW', 'W', 'NW')[round(packet.track/45.0) % 8]
         
-        lcd.write('%3.0f %-4.4s %-2.2s' % (speed, units, direction))
+        lcd.write('%3.0f %s %-2.2s' % (speed, speed_units, direction))
 
         precision = packet.position_precision()
         lcd.set_cursor_position(11, 1)
-        if USE_METRIC:
-            lcd.write(chr(0xf9)+'%3.0fm' % precision[0])
-        else:
-            lcd.write(chr(0xf9)+"%3.0f'" % (precision[0] * 3.28084))
+        lcd.write(chr(0xf9)+'%3.0f%s' % (precision[0] * distance_factor,
+                                         distance_units))
 
+        # Convert this to a vague signal quality metric
+        # Use sqrt(1/HDOP) instead?
+        
         pval = math.expm1(math.e/(precision[0]+1))
         backlight.set_graph(pval)
         #print(precision[0], pval)
         
         if packet.mode >= 3:
             lcd.set_cursor_position(10, 0)
-            if USE_METRIC:
-                lcd.write('%5.0fm' % packet.alt)
-            else:
-                lcd.write("%5.0f'" % (packet.alt * 3.28084))
+            lcd.write('%5.0f%s' % (packet.alt * distance_factor,
+                                   distance_units))
         else:
-            lcd.set_cursor_position(14, 0)
-            lcd.write('2D')
+            lcd.set_cursor_position(13, 0)
+            lcd.write('2-D')
             
     else:
         lcd.clear()
